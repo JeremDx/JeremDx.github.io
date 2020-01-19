5ec6850cf0931dac4d1c62c25f72829b4cce0935
kevinhwcj <48894711+kevinhwcj@users.noreply.github.com>
2020-01-19 19:02:07 +0100

    ejs + css

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-let app = require('express')()
+let express = require('express');
+let app = express()
 app.set('view engine','ejs');
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('pages/index');
